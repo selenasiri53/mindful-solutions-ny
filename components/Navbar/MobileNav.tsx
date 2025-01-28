c33ee5f3a7293e7ceca56efb86067b471a76efe4
Selena Siri <selenasiri53@gmail.com>
2025-01-27 19:16:56 -0500

    signup

--- components/Navbar/MobileNav.tsx
+++ components/Navbar/MobileNav.tsx
@@ -59,7 +59,7 @@ export const MobileNav = ({ navItems }: any) => {
             ))}
             <motion.button
               variants={childItems}
-              onClick={() => router.push("/contact")}
+              onClick={() => router.push("/signup")}
               className="text-white inline-flex items-center justify-center rounded-[10px] bg-gradient-to-b from-[#464d55] to-[#25292e] text-sm px-4 py-2 shadow-[0_10px_20px_rgba(0,_0,_0,_.1),0_3px_6px_rgba(0,_0,_0,_.05)] hover:shadow-[rgba(0,_1,_0,_.2)_0_2px_8px] active:outline-none hover:opacity-80 "
             >
               Contact
